1dd9febe80d4d9b3d179b288c2b8addc8dba702f
nginrsw <nginrsw@gmail.com>
2024-12-21 09:08:37 +0700

    dirdel&&additm

---
+++ elixir/pattern_s.exs
@@ -0,0 +1,5 @@
+x = {1,2}
+# {x,y} = {1,2}
+
+IO.inspect x
+# IO.puts("#{x}, #{y}")
--- ocaml/hello.ml
+++
@@ -1,6 +0,0 @@
-let say_hello () =
-	print_endline "Hello World!";;	
-	print_endline "It Works!!";;
-	
-(*panggil fungsi*)
-say_hello ();;	
