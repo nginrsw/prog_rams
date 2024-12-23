9a274d8ae7dc6355f52f14a1b27f7c70564c31de
nginrsw <nginrsw@gmail.com>
2024-12-24 06:45:27 +0700

    delitms&&additms

--- python/app.py
+++ python/app.py
@@ -1,40 +1,58 @@
-print("Hello World!")
+def main():
 
-#-------> tes factorial
-print("faktorial")
-print("---------------")
-def fact(n):
-    if n == 0:
-        return 1
-    else:
-        return n * fact(n - 1)
-        
-#-------> tes input fn fact
-fact_inpt = input("masukan nomer fact ")
-print("---------------")
-print(fact(int(fact_inpt)))
+    print("Hello World!")
+    print("It Works!!")
 
-#-------> tes loop pake rekursi biasa
-print("perulangan rekursi")
-print("---------------")
-def count(n):
-    if n > 10:
-        return
-    print(n)
-    count(n+1)
-        
-count_inpt1 = input("masukan nomer count ")
-print("---------------")
-count(int(count_inpt1))
+    #-------> tes factorial
+    print("faktorial")
+    print("---------------")
+    def fact(n):
+        if n == 0:
+            return 1
+        else:
+            return n * fact(n - 1)
+            
+    #-------> tes input fn fact
+    fact_inpt = input("masukan nomer fact ")
+    print("---------------")
+    print(fact(int(fact_inpt)))
 
-#-------> tes loop pake rekursi dengan bantuan list
-print("perulangan rekursi")
-print("---------------")
-def count(n):
-    if n > 10:
-        return []
-    return [n] + count(n + 1)
+    #-------> tes loop pake rekursi biasa
+    print("perulangan rekursi")
+    print("---------------")
+    def count(n):
+        if n > 10:
+            return
+        print(n)
+        count(n+1)
+            
+    count_inpt1 = input("masukan nomer count count start loop (dibawah angka 10) ")
+    print("---------------")
+    count(int(count_inpt1))
 
-count_inpt2 = input("masukan nomer count list ")
-print("---------------")
-print(count(int(count_inpt2)))
+    #-------> tes loop pake rekursi dengan bantuan list
+    print("perulangan rekursi")
+    print("---------------")
+    def count(n):
+        if n > 10:
+            return []
+        return [n] + count(n + 1)
+
+    count_inpt2 = input("masukan nomer count start loop w/ bantuan list (dibawah angka 10) ")
+    print("---------------")
+    print(count(int(count_inpt2)))
+
+    #-------> tes fibonacci menggunakan rekursi
+    print("fibonacci")
+    print("---------------")
+
+    def fib(n):
+        if n <= 1:
+            return n
+        return fib(n-1) + fib(n-2)
+
+    fib_inpt = input("masukan nomer fib ")
+    print(fib(int(fib_inpt)))
+    
+if __name__ == "__main__":
+    main()
